3f9dd0f555b66e2c6e4749d3282738c65dfee173
Carlos Villanueva <75815689+carvillanueva@users.noreply.github.com>
2024-08-03 15:48:55 -0400

    test PR rule (#2)

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Carlos Animation Site",
+  title: "Carlos Animation Site Example",
   description:
     "Practice animations with Framer Motion + Next.js + Aceternity UI",
 };

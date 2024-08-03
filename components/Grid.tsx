8b02a635215a9392abc6aa958b0cdf015a86ca26
Carlos Villanueva <75815689+carvillanueva@users.noreply.github.com>
2024-08-03 15:37:41 -0400

    Bento grid update (#1)
    * update bento grid stucture
    
    * add all data stucutres into index
    
    * add all needed assets files
    
    * draft of bento grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,18 +1,37 @@
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
 
 const Grid = () => {
-  return <section id="about" className="">
-    <BentoGrid>
-      {[{title: 'Title',description: 'Desc1'}].map((item, index) => (
-        // <BentoGridItem
-        //   key={index}
-        //   title={item.title}
-        //   description={item.description}
-        //   icon={<FaLocationArrow />}
-        // />
-      ))}
-    </BentoGrid>
-  </section>;
+  return (
+    <section id="about" className="">
+      <BentoGrid>
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  );
 };
 
 export default Grid;

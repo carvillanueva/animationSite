11cd49b26b973edb3cfd13d82b43ff5bc49f0494
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-08-04 17:18:18 -0400

    fix projects size

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -115,7 +115,6 @@ export const BentoGridItem = ({
 
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-              {/* tech stack lists */}
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
                 {leftLists.map((item, i) => (
                   <span
